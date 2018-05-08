813658e1e094203be4746e713f80ba7392083662
clarkbaum <nbaumg@hotmailcom>
2018-05-08 15:30:09 -0500

    auto formatting

--- src/main/java/com/boot/config/PersistenceConfiguration.java
+++ src/main/java/com/boot/config/PersistenceConfiguration.java
@@ -11,19 +11,19 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-//	bean lets spring boot know the output of this function needs to be a bean (DataSource)
+	//	bean lets spring boot know the output of this function needs to be a bean (DataSource)
 	@Bean
 	//configprop lets them know the properties in application.properties file that starts with spring.datasource is needed for this
-	@ConfigurationProperties(prefix="spring.datasource")
+	@ConfigurationProperties(prefix = "spring.datasource")
 	//sets the default data source if none else is specified
 	@Primary
-	public DataSource dataSource(){
+	public DataSource dataSource() {
 		return DataSourceBuilder.create().build();
 	}
 
 	//2nd bean using another database
 	@Bean
-	@ConfigurationProperties(prefix="datasource.flyway")
+	@ConfigurationProperties(prefix = "datasource.flyway")
 	@FlywayDataSource //flyway specifc flag
 	public DataSource flywayDataSource() {
 		return DataSourceBuilder.create().build();
